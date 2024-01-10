d2cffeecfd74f5b2e1fd101370fbd9f7104ca506
Jordan Cox <jordancox747@outlook.com>
2024-01-09 18:45:13 -0700

    begin setting up server.ts file

--- server.ts
+++ server.ts
@@ -1,3 +1,14 @@
 import express = require('express');
 import mongoose = require('mongoose');
+import cors = require('cors');
+import passport = require('passport');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+
 
