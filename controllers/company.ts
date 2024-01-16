45d00f51335ea2fa8a11203244e7996bb20f5fbc
Jordan Cox <jordancox747@outlook.com>
2024-01-16 07:43:16 -0700

    company controller file created

---
+++ controllers/company.ts
@@ -0,0 +1,7 @@
+// once initial admin has signed up, they must create a company profile
+
+// edit company profile
+
+// change roles for company profile (who is an admin / user)
+
+// delete company
