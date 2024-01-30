8a318c8c56fb37692a2f435cdd3e60fa2bfcbff6
Jordan Cox <jordancox747@outlook.com>
2024-01-30 11:47:19 -0700

    pending admin approval for email

--- services/nodemailer.ts
+++ services/nodemailer.ts
@@ -6,16 +6,29 @@ import nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: 'smtp.forwardemail.net',
   port: 587,
-  secure: false,
+  secure: false, // development
   auth: {
     user: process.env.FORWARD_EMAIL,
     pass: process.env.FORWARD_PASSWORD,
   },
 });
 
+const sendInvite = async (
+  invitee: { email: string; name: string },
+  companyName: string,
+) => {
+  const info = await transporter.sendMail({
+    from: '"Project Core" <noreply@projectcore.co>',
+    to: invitee.email,
+    subject: `You've been invited to join ${companyName}'s Project Core team`,
+    text: `Hello, ${invitee.name}, you've beeen invited to join ${companyName} on Project Core [insert link here]. Click here and follow the instructions on the web page.`,
+    html: `<p>Hello, ${invitee.name}, you've beeen invited to join ${companyName} on Project Core [insert link here]. Click here and follow the instructions on the web page.</p>`, // eventually insert link for them to click
+  });
+};
+
 async function main() {
   const info = await transporter.sendMail({
-    from: '"Jordan Cox" <noreply@projectco.co>',
+    from: '"Jordan Cox" <noreply@projectcore.co>',
     to: 'jordancox747@outlook.com',
     subject: 'HEllo',
     text: 'Hello world',
